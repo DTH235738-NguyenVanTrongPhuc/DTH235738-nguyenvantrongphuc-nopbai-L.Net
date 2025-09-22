962c7b6ef57658633fbbf70333c27b91ae5cd6ff
hc <pphhzz1122@gmail.com>
2025-09-22 20:26:11 +0700

    cac bai tap thuc hanh cua l.net

---
+++ BuoiTH3/Bai3/Form1.cs
@@ -0,0 +1,27 @@
+﻿namespace Bai3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnok_Click(object sender, EventArgs e)
+        {
+            
+            this.txtkq.Text = "Bạn đã chọn website ";
+            this.txtkq.Text += this.lstname.SelectedItems[0].Text;
+
+
+        }
+        private void txtkq_TextChanged(object sender, EventArgs e)
+        {
+            this.txtkq.Clear();
+        }
+    }
+}
